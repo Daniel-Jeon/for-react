5099b3a3103f92c66bcf515a5d15e080066b9932
Daniel Jeon <45351514+Daniel-Jeon@users.noreply.github.com>
2024-12-09 15:12:51 +0900

    to do list for react (1)
    yas

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
-  useEffect(() => console.log("call the api"), []);
-  useEffect(() => {
-    console.log("i run when 'keyword' change");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("i run when 'counter' change");
-  }, [counter]);
-  useEffect(() => {
-    console.log("i run when 'both' change");
-  }, [keyword, counter]);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") return;
+    setTodos((current) => [toDo, ...current]);
+    setTodo("");
+  };
+  console.log("toDos:", toDos);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search Here"
-      />
-      <h1>Welcome Back {counter}</h1>
-      <button onClick={onClick}></button>
+      <h1>My To Dos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
