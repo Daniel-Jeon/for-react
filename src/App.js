3e083f7057d5f24b44fdccd39bf1abfb29a5fccc
Daniel Jeon <45351514+Daniel-Jeon@users.noreply.github.com>
2024-12-09 10:40:00 +0900

    useEffect deps
    how

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+  useEffect(() => console.log("call the api"), []);
+  useEffect(() => {
+    console.log("i run when 'keyword' change");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when 'counter' change");
+  }, [counter]);
+  useEffect(() => {
+    console.log("i run when 'both' change");
+  }, [keyword, counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back</h1>
-      <Button text={"Click"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search Here"
+      />
+      <h1>Welcome Back {counter}</h1>
+      <button onClick={onClick}></button>
     </div>
   );
 }
